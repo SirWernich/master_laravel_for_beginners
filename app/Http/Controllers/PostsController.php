3af2c407703781e244bd0421677871693090fb80
Wernich Wilsenach <melktert@gmail.com>
2021-01-23 19:51:07 +0200

    Update cache to use tags
    Update cache usage to use tags.
    Add cache flush to seeding.

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -25,7 +25,7 @@ class PostsController extends Controller
      */
     public function index()
     {
-        $mostCommented = Cache::remember('blog-post-most-commented', now()->addMinutes(10), function () {
+        $mostCommented = Cache::tags(['blog-post'])->remember('blog-post-most-commented', now()->addMinutes(10), function () {
             return BlogPost::mostCommented()->take(5)->get();
         });
 
@@ -119,7 +119,7 @@ class PostsController extends Controller
         //     ], )->findOrFail($id)
         // ]);
 
-        $blogPost = Cache::remember("blog-post-{$id}", 60, function () use ($id) {
+        $blogPost = Cache::tags(['blog-post'])->remember("blog-post-{$id}", 60, function () use ($id) {
             return BlogPost::with('comments')->findOrFail($id);
         });
 
@@ -127,7 +127,7 @@ class PostsController extends Controller
         $counterKey = "blog-post-{$id}-counter";
         $usersKey = "blog-post-{$id}-users";
 
-        $users = Cache::get($usersKey, []);
+        $users = Cache::tags(['blog-post'])->get($usersKey, []);
         $usersUpdate = [];
         $difference = 0;
         $now = now();
@@ -148,15 +148,15 @@ class PostsController extends Controller
         }
 
         $usersUpdate[$sessionId] = now();
-        Cache::forever($usersKey, $usersUpdate);
+        Cache::tags(['blog-post'])->forever($usersKey, $usersUpdate);
 
-        if (!Cache::has($counterKey)) {
-            Cache::forever($counterKey, 1);
+        if (!Cache::tags(['blog-post'])->has($counterKey)) {
+            Cache::tags(['blog-post'])->forever($counterKey, 1);
         } else {
-            Cache::increment($counterKey, $difference);
+            Cache::tags(['blog-post'])->increment($counterKey, $difference);
         }
 
-        $counter = Cache::get($counterKey);
+        $counter = Cache::tags(['blog-post'])->get($counterKey);
 
 
         return view('posts.show', [
