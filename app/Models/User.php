9e7f4e705312c5a47ad194febd52055961dfebae
Wernich Wilsenach <melktert@gmail.com>
2021-02-13 22:22:31 +0200

    Create 1 to 1 polymorphic relationship for Image
    Create 1 to 1 polymorphic relationship for Image and User/BlogPost.
    Add logic to save and update avatar for users.

--- app/Models/User.php
+++ app/Models/User.php
@@ -64,6 +64,11 @@ class User extends Authenticatable
         return $this->hasMany(\App\Models\Comment::class);
     }
 
+    public function image()
+    {
+        return $this->morphOne(Image::class, 'imageable');
+    }
+
     public function scopeWithMostBlogPosts(Builder $query)
     {
         return $query->withCount('blogPosts')
--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -9,7 +9,7 @@
 
         <div class="row">
             <div class="col-4">
-                <img src="" class="img-thumbnail avatar" />
+                <img src="{{ $user->image ? $user->image->url() : '' }}" class="img-thumbnail avatar" />
                 <div class="card mt-4">
                     <h6>Upload a new photo</h6>
                     <input class="form-control-file" type="file" name="avatar">
@@ -20,12 +20,15 @@
                     <label>Name:</label>
                     <input type="text" name="name" class="form-control">
                 </div>
+
+                @errors
+                @enderrors
+
                 <div class="form-group">
                     <input type="submit" value="Save Changes" class="btn btn-primary">
                 </div>
             </div>
         </div>
     </form>
-
 
 @endsection
