db0960bcb336479b88a7d537a6bc919ddbf4890b
Wernich Wilsenach <melktert@gmail.com>
2021-03-16 09:37:23 +0200

    Change from dependency injection to facade
    Create CounterFacade to replace dependency injection of Counter

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Contracts\CounterContract;
 use App\Events\BlogPostPosted;
+use App\Facades\CounterFacade;
 use App\Http\Requests\StorePost;
 use App\Models\BlogPost;
 use App\Models\Image;
@@ -14,13 +14,10 @@ use Illuminate\Support\Facades\Storage;
 
 class PostsController extends Controller
 {
-    protected $counter;
-
-    public function __construct(CounterContract $counter)
+    public function __construct()
     {
         $this->middleware('auth')
             ->only(['create', 'edit', 'destroy', 'store', 'update']);
-        $this->counter = $counter;
     }
 
     /**
@@ -98,10 +95,9 @@ class PostsController extends Controller
                 ->findOrFail($id);
         });
 
-        dd($this->counter);
         return view('posts.show', [
             'post' => $blogPost,
-            'counter' => $this->counter->increment("blog-post-{$id}", ['blog-post'])
+            'counter' => CounterFacade::increment("blog-post-{$id}", ['blog-post'])
         ]);
     }
 
